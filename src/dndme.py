5157b69a7ec5fdb62fe595f010b8a871332fa2f0
Matthew McIlree <mjm42@st-andrews.ac.uk>
2020-03-07 19:46:03 +0000

    Started pipelining together

--- src/dndme.py
+++ src/dndme.py
@@ -1,6 +1,9 @@
 from src.imagemeasure import initial_form, measurement_picker
+from src.monsters import monstercomparison
 
 name = initial_form.survey()
 front_path = initial_form.select_front_image()
 side_path = initial_form.select_side_image()
 measurement_picker.make_measurements(front_path, side_path, name)
+
+monstercomparison.makeMonster()
