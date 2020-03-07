64f2d6cae287f474ccb663bb9a97426bedbdab62
Carson Brown <cmb38@st-andrews.ac.uk>
2020-03-07 14:13:28 +0000

    Ratios for strength and constitution scores.

--- statgen/statGenerator.py
+++ statgen/statGenerator.py
@@ -1,22 +1,35 @@
 import numpy as np
 import json
+import math
+from person import Person
 
 def main():
     genStats()
 
 def genStats():
     filename = "JSON/" + input("Enter JSON file: ") + ".json"
-    print("Reading from: " + filename)
 
     with open(filename, 'r') as file:
         data = file.read().replace('\n', '')
 
-    print(data)
-    print("\n")
-
     stats = json.loads(data)
 
     print(stats)
+    person = Person(stats)
+
+    print(str(person))
+
+    STR = genStrength(person)
+    CON = genConstitution(person)
+    print(STR, CON)
+
+def genStrength(person):
+    strength = 20 - (math.log(person.strDelta + 1) * 3) ** 4
+    return round(strength)
+
+def genConstitution(person):
+    strength = 20 - (math.log(person.strDelta + 1) * 3) ** 4
+    return round(strength)
 
 if __name__== "__main__":
     main()
